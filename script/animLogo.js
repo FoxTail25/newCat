a1b2db9d3bb00d79b070b0456c478d828909be04
Grigoriy <foxtail25@gmail.com>
2022-10-29 14:28:46 +0300

    стилизация карточек

--- script/animLogo.js
+++ script/animLogo.js
@@ -4,13 +4,17 @@ const logoLink = document.querySelector('.logo');
 // class Logo {
 //     constructor()
 
+logo.style.color = 'white'
 
     function changeOver() {
         logo.className = 'fa-solid fa-paw';
+        logo.style.color = 'black'
+        
     }
 
     function changeLiave() {
         logo.className = 'fa-solid fa-cat';
+        logo.style.color = 'white'
     }
 
 
--- script/popup.js
+++ script/popup.js
@@ -18,23 +18,18 @@ class Popup {
 
     open() {
         const isAuth = Cookies.get('email');
-        // let a = isAuth
-        // console.log(a === '')
+    
         console.log(isAuth)
         if(this._className != 'popup-login' && !isAuth) {
             alert('Для добавления и изменения данных необходима авторизация')
             popupAddCat.close()
             auth.open()
-        //     // console.log("true")
-        //     // authTrue()
+   
 
         } else {
         this.popup.classList.add('popup_active');
         document.addEventListener('keyup', this._handleEscUp)
-        // }
-        // let el = document.querySelector('.`${this._className}`')
-        // let el2 = el.document.querySelector('div')
-        // console.log(typeof el)
+      
     }
 }
 
