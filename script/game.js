1e9b617ba7f22636a8af7f281a93d401f45463d8
Grigoriy <foxtail25@gmail.com>
2023-02-07 14:26:47 +0300

    Изменения отображения счетчика

--- script/game.js
+++ script/game.js
@@ -3,27 +3,18 @@
 
 export function handelFish() {
     const fishElement = document.querySelectorAll('.fish')
-    // console.log(fishElement)
     for (let e of fishElement) {
-        // let cnt = 0
         e.addEventListener('click', (e) => {
-
-          count()  
-
+            
+            count()
             let a = e.target.parentElement
             a.classList += " invisible";
             setTimeout(() => {
-                
 
                 return a.classList.remove("invisible")
-                // console.log(a)
+
             }, 3000)
-            
         }
-        // console.log('bingo',a);
-        //  e.path[1].classList
-        //    console.log(e.path[1].classList.value)
-        // console.log(e.path)
         )
     }
 }
@@ -32,35 +23,27 @@ let a = 0
 const luckyFishCount = document.querySelector('.count')
 
 function count() {
-   
-a++
-console.log(a)
-luckyFishCount.innerHTML = `Вы поймали ${a}`
 
-switch (a) {
-    case 2: alert("Мои поздравления вы поймали больше одной рыбки!");
-    break
-    case 5: alert("Браво! Уже 5");
-    break
-    case 9: alert("9! Да вы прирожденный рыбак!");
-    break
-    case 15: alert("Глядя на ваши успехи, профессиональные рыбаки нервно курят в сторонке и подумывают стать программистами");
-    break
-    case 20: alert("Это новый рекорд! Вы превзошли все ожидания))");
-    break
+    a++
+
+    luckyFishCount.innerHTML = `<div> Вы поймали ${a} <i class="fa-solid fa-fish-fins fish_for_count"></i></div>`;
+
+    switch (a) {
+        case 2: alert("Мои поздравления вы поймали больше одной рыбки!");
+            break
+        case 5: alert("Браво! Уже 5");
+            break
+        case 9: alert("9! Да вы прирожденный рыбак!");
+            break
+        case 15: alert("Глядя на ваши успехи, профессиональные рыбаки нервно курят в сторонке и подумывают стать программистами");
+            break
+        case 20: alert("Это новый рекорд! Вы превзошли все ожидания))");
+            break
+        case 30: alert("Рыбу можно ловить бесконечно!))");
+            break
+    }
+
+
+    return a
+
 }
-
-
-return a
- 
-}
-
-
-// отображени количества пойманых рыбок
-
-
-// function fishCount() {
-    // let am = cnt3
-    // console.log(cnt)
-    // luckyFishCount.innerText = `Вы поймали ${am}`
-// }
